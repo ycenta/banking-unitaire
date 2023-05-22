45ee4d257f2785a729f45c12b03dc43df0cf0e8c
Yohan Centanni <thorbenliya@gmail.com>
2023-05-22 15:06:20 +0200

    add remove add money functions

--- index.js
+++ index.js
@@ -17,3 +17,42 @@ app.get("/users/:id/accounts", (req, res) => {
 });
 
 app.listen(3000, () => console.log("Server started on port 3000"));
+
+function addMoney(bankaccount, money) {
+
+    let balance = bankaccount.balance;
+    let rest = 0;
+    if (balance + money > 1000) {
+       rest = (balance + money) - 1000;
+    }
+
+    let newBalance = balance + money - rest;
+    bankaccount.balance = newBalance;
+
+    return {
+        moneyAdded: money - rest,
+        rest: rest,
+        balance: newBalance
+    }
+
+}
+
+function removeMoney(bankaccount, money) {
+
+        let balance = bankaccount.balance;
+        let rest = 0;
+        if(balance - money < 0) {
+            rest = money - balance;
+            rest = rest * -1;            
+        }
+
+        let newBalance = balance - money + rest;
+        bankaccount.balance = newBalance;
+
+        return {
+            moneyRemoved: money - rest,
+            rest: rest,
+            balance: newBalance
+        }
+    
+    }
