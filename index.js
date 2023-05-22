5463138681ae0d6494c65f71103cfbff08eb791f
Samuel GUENIER <guenier.samuel@gmail.com>
2023-05-22 12:27:49 +0200

    Adding basic expressjs thingy

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello')
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.put("/users/:id/accounts/debit", (req, res) => {
+    res.send("Hello World");
+});
+
+app.put("/users/:id/accounts/credit", (req, res) => {
+    res.send("Hello World");
+});
+
+app.get("/users/:id/accounts", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(3000, () => console.log("Server started on port 3000"));
